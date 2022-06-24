b3a6b9ed2298ba177cb7b1e2ee21eb9df8473f95
KamilAugustyn22 <augustynkamil15@gmail.com>
2022-06-24 15:17:35 +0200

    Wstęp - repaired

--- src/main/java/pl/coderslab/beans/Book.java
+++ src/main/java/pl/coderslab/beans/Book.java
@@ -16,4 +16,41 @@ public class Book {
         this.publisher = publisher;
         this.type = type;
     }
+    public void setId(long id){
+        this.id = id;
+    }
+    public long getId(){
+        return id;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    public String getIsbn(){
+        return isbn;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public void setAuthor(String author){
+        this.author = author;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public void setPublisher(String publisher){
+        this.publisher = publisher;
+    }
+    public String getPublisher(){
+        return publisher;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
+    public String getType(){
+        return type;
+    }
 }
